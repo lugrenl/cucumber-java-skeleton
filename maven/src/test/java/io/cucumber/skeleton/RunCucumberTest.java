513c59d4a9ed39cce2a3b353255fc926e0a7ef3a
LugrenLaxx <lugrenl@gmail.com>
2025-04-14 23:31:14 +0300

    add_allure_report

--- maven/src/test/java/io/cucumber/skeleton/RunCucumberTest.java
+++ maven/src/test/java/io/cucumber/skeleton/RunCucumberTest.java
@@ -8,7 +8,10 @@ import static io.cucumber.junit.platform.engine.Constants.*;
 @IncludeEngines("cucumber")
 @SelectPackages("io.cucumber.skeleton")
 @IncludeTags({"Hungry", "Full"})
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, " +
+        "summary, " +
+        "json:target/reports/cucumber-reports/cucumber.json, " +
+        "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "io.cucumber.skeleton")
 // @ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "not @hungry and not @full")
 public class RunCucumberTest {
